cfababa6f1c3a679d8da4e9a42bc871ea2227b75
Valinda Chan <valinda.chan@gmail.com>
2017-08-01 08:56:40 -0400

    Clean up code

--- app.js
+++ app.js
@@ -10,53 +10,18 @@ app.engine("mustache", mustacheExpress())
 app.set("views", "./views")
 app.set("view engine", "mustache")
 
-// Dummy data of todos
-let todos = {
-  todos: [
-    { id: 0, item: "Pick up dog food", status: "complete" },
-    { id: 1, item: "Paint the bathroom", status: "incomplete" },
-    { id: 2, item: "Go to Canada", status: "incomplete" },
-    { id: 3, item: "Check out new vegan burger place", status: "complete" }
-  ]
-}
-
-// Dummy list of user details for testing
-let users = {
-  users: [
-    { id: 0, user_id: "username", password: "password1" },
-    { id: 1, user_id: "username2", password: "password2" }
-  ]
-}
-
-// console.log(users)
-
-// Find another data structure to use but still know how to get this
-// to work with Mustache?
-let currentUser = { currentUser: [{ currentUser: "" }] }
-
-// const authenticate = (req, res, next) => {
-//   currentUser.currentUser[0].currentUser = req.query.user_id
+// Dummy list of user details for testing but this isn't used
+// Technically should be in a database and not here
 //
-//   console.log("hiiiiii")
-//
-//   for (var i = 0; i < users.users.length; i++) {
-//     if (currentUser.currentUser[0].currentUser === users.users[i].user_id) {
-//       console.log("yay")
-//       next()
-//     } else {
-//       res.redirect("/login")
-//     }
-//   }
-//
-//   // if (req.query.user_id === "username" && req.query.pw === "password") {
-//   //   next()
-//   // } else {
-//   //   res.redirect("/login")
-//   // }
+// let users = {
+//   users: [
+//     { id: 0, user_id: "username", password: "password1" },
+//     { id: 1, user_id: "username2", password: "password2" }
+//   ]
 // }
 
 const authenticate = (req, res, next) => {
-  currentUser.currentUser[0].currentUser = req.query.user_id
+  currentUser = req.query.user_id
   if (req.query.user_id === "username" && req.query.pw === "password") {
     next()
   } else {
@@ -70,23 +35,10 @@ app.get("/login", function(req, res) {
 
 app.use(authenticate)
 app.get("/", function(req, res) {
-  let completedItems = todos.todos.filter(todo => todo.status === "complete")
-  let incompleteItems = todos.todos.filter(todo => todo.status === "incomplete")
-
-  let listByCompletion = {
-    complete: [],
-    incomplete: [],
-    currentUser: currentUser.currentUser[0].currentUser
+  let currentData = {
+    currentUser: currentUser
   }
-
-  listByCompletion.complete = completedItems
-  listByCompletion.incomplete = incompleteItems
-
-  res.render("index", listByCompletion)
-})
-
-app.get("/", function(req, res) {
-  res.render("index", currentUser)
+  res.render("index", currentData)
 })
 
 app.listen(3000, function() {
